179341755417e6288316a2624781510d2ddaa784
cao8280403 <386752435@qq.com>
2019-04-23 15:54:55 +0800

    导出表格功能

--- src/main/java/com/webLinkSystem/houtai/controller/ControllerOfRegisterHistory.java
+++ src/main/java/com/webLinkSystem/houtai/controller/ControllerOfRegisterHistory.java
@@ -6,9 +6,13 @@ import com.webLinkSystem.houtai.bean.WebLink;
 import com.webLinkSystem.houtai.service.ServiceOfRegisterHistory;
 import com.webLinkSystem.houtai.service.ServiceOfVisitHistory;
 import com.webLinkSystem.houtai.service.ServiceOfWebLink;
+import org.apache.poi.hssf.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +28,7 @@ public class ControllerOfRegisterHistory {
     @Autowired
     private ServiceOfRegisterHistory serviceOfRegisterHistory;
 
-//    @ResponseBody
+    //    @ResponseBody
 //    @RequestMapping(value = "/findAll", method = RequestMethod.POST,produces="application/json;charset=UTF-8")
 //    private String findAllweblink(@RequestParam Map<String, String> reqMap) {
 //        String productName =reqMap.get("productName");
@@ -46,24 +50,48 @@ public class ControllerOfRegisterHistory {
 //
 //
 //
-//    @RequestMapping(value = "/findWeblinkByGiud" , method = RequestMethod.POST)
-//    private String findWeblinkByGiud(@RequestParam Map<String, String> reqMap) {
-//        Integer guid = Integer.valueOf(reqMap.get("guid"));
-//        String flag = "";
-//        List<WebLink> list = serviceOfWebLink.findByGuid(guid);
-//        if(list.size()==0){
-//            flag = "此链接不存在";
-//        }else{
-//            Integer state = list.get(0).getState();
-//            String sourceWebLink = list.get(0).getSourceWebLink();
-//            if (state==1){
-//                flag = sourceWebLink;
-//            }else{
-//                flag = "此链接已关闭";
-//            }
-//        }
-//        return flag;
-//    }
+    @RequestMapping(value = "/daochu", method = RequestMethod.GET)
+    private void daochu(@RequestParam String guid,@RequestParam String year,@RequestParam String month,HttpServletResponse response) throws IOException {
+        String guidlike = guid + "%";
+        List<RegisterHistory> list = serviceOfRegisterHistory.getDaochuList(guidlike, year, month);
+        HSSFWorkbook shengchengexcel = shengchengexcel(list);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename=" + guid+"-"+year+"-"+month+".xls");
+        response.flushBuffer();
+        shengchengexcel.write(response.getOutputStream());
+    }
+
+    //生成excel表格
+    public HSSFWorkbook shengchengexcel(List<RegisterHistory> list) {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("登记列表");
+        String fileName = "registerHistory" + ".xls";//设置要导出的文件的名字
+        //新增数据行，并且设置单元格数据
+        int rowNum = 1;
+        String[] headers = {"id", "登记时间", "姓名", "手机号码", "产品id", "ip"};
+        //headers表示excel表中第一行的表头
+        HSSFRow row = sheet.createRow(0);
+        //在excel表中添加表头
+        for (int i = 0; i < headers.length; i++) {
+            HSSFCell cell = row.createCell(i);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            cell.setCellValue(text);
+        }
+        if (list.size() > 0) {
+            //在表中存放查询到的数据放入对应的列
+            for (RegisterHistory registerHistory : list) {
+                HSSFRow row1 = sheet.createRow(rowNum);
+                row1.createCell(0).setCellValue(registerHistory.getGuid());
+                row1.createCell(1).setCellValue(registerHistory.getCreatetime());
+                row1.createCell(2).setCellValue(registerHistory.getName());
+                row1.createCell(3).setCellValue(registerHistory.getTelephone());
+                row1.createCell(4).setCellValue(registerHistory.getProductName());
+                row1.createCell(5).setCellValue(registerHistory.getIp());
+                rowNum++;
+            }
+        }
+        return workbook;
+    }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     private String insertWeblink(@RequestParam Map<String, String> reqMap) {
--- src/main/java/com/webLinkSystem/houtai/service/ServiceOfRegisterHistory.java
+++ src/main/java/com/webLinkSystem/houtai/service/ServiceOfRegisterHistory.java
@@ -11,6 +11,9 @@ import java.util.List;
 public interface ServiceOfRegisterHistory extends JpaRepository<RegisterHistory, String> {
     List<RegisterHistory> findByProductNameAndIp(String productName, String ip);
 
+    @Query(value = "select * from register_history where year(createtime) = ?2 and month(createtime) = ?3 and product_name like ?1",nativeQuery = true)
+    List<RegisterHistory> getDaochuList(String guid, String year, String month);
+
 
 //    @Query(value = "SELECT * FROM web_link WHERE product_name LIKE ?1 ORDER BY create_time DESC ",nativeQuery = true)
 //    List<VisitHistory> findAllOrderByCreateTimeDesc(String productName);
